4751c0c7f72ba7fe8be7fa80b691d1e317853dd2
dmsqja <bum17822@naver.com>
2026-08-31 14:10:02 +0900

    chore: 커밋 타입에 test, refactor 추가

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": [2, "always", ["feat", "fix", "chore"]],
+    "type-enum": [2, "always", ["feat", "fix", "chore", "test", "refactor"]],
+    "subject-case": [0],
   },
 };
