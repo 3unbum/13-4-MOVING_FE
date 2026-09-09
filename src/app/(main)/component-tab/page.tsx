b81c02fd27263937ef2b75876c01d71b5e210053
MunChiho <mch52373820@gmail.com>
2026-09-09 11:37:27 +0900

    feat: 공통 컴포넌트 Tab 작업 (#38)
    * feat: 공통 컴포넌트 Tab 구현
    
    * fix: Tab 반응형 스타일 및 TabList 추가
    
    * fix: 코드래빗 리뷰 적용 및 수정
    
    * fix: Tab 색상 토큰 Figma 값에 맞게 수정

---
+++ src/app/(main)/component-tab/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useState } from "react";
+import Tab from "@/component/common/tab";
+import TabList from "@/component/common/tab-list";
+
+export default function TabPage() {
+  const [quoteTab, setQuoteTab] = useState<"waiting" | "received">("waiting");
+
+  return (
+    <div>
+      <p className="text-14 p-4 font-semibold text-gray-500">브라우저 폭을 조정해서 확인해보세요</p>
+
+      <TabList aria-label="견적 탭">
+        <Tab
+          id="tab-waiting"
+          controls="panel-waiting"
+          active={quoteTab === "waiting"}
+          onClick={() => setQuoteTab("waiting")}
+        >
+          대기 중인 견적
+        </Tab>
+        <Tab
+          id="tab-received"
+          controls="panel-received"
+          active={quoteTab === "received"}
+          onClick={() => setQuoteTab("received")}
+        >
+          받았던 견적
+        </Tab>
+      </TabList>
+
+      {quoteTab === "waiting" && (
+        <div
+          id="panel-waiting"
+          role="tabpanel"
+          aria-labelledby="tab-waiting"
+          tabIndex={0}
+          className="text-14 p-4 text-gray-500"
+        >
+          대기 중인 견적 목록 (예시 콘텐츠)
+        </div>
+      )}
+      {quoteTab === "received" && (
+        <div
+          id="panel-received"
+          role="tabpanel"
+          aria-labelledby="tab-received"
+          tabIndex={0}
+          className="text-14 p-4 text-gray-500"
+        >
+          받았던 견적 목록 (예시 콘텐츠)
+        </div>
+      )}
+    </div>
+  );
+}
