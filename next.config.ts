b3169e07f302a2a56a15915e247937070dc19036
singsangsong28 <kuu052888@gmail.com>
2026-09-21 01:46:47 +0900

    fix: aws 배포 후 프론트에 연결을 위한 next.config 수정

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,16 @@
-import type { NextConfig } from "next";
-
-const BACKEND_ORIGIN = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001";
-
-if (!process.env.NEXT_PUBLIC_S3_IMAGE_HOST) {
-  throw new Error("NEXT_PUBLIC_S3_IMAGE_HOST 환경변수가 설정되지 않았습니다.");
-}
-const S3_IMAGE_HOST = process.env.NEXT_PUBLIC_S3_IMAGE_HOST;
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: ["172.30.1.25"],
   reactCompiler: true,
-  // 네트워크를 통한 테스트 서버
-  allowedDevOrigins: ["172.30.1.15"],
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: S3_IMAGE_HOST,
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
       },
     ],
   },
@@ -24,7 +18,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${BACKEND_ORIGIN}/api/:path*`,
+        destination: "http://3.34.7.94/api/:path*",
       },
     ];
   },
